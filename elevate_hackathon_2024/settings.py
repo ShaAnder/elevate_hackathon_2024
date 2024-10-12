c1f2326dca315d753be596b1c08e1c40526663f1
Viktor Rura <ruravictor@gmail.com>
2024-10-12 07:41:19 +0100

    Add heroku url to allowed hosts

--- elevate_hackathon_2024/settings.py
+++ elevate_hackathon_2024/settings.py
@@ -17,7 +17,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "job-me-c2269b1b44b0.herokuapp.com",
+]
 
 AUTH_USER_MODEL = "users.CustomUser"
 
