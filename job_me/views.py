af2de8ed863177553a3ad39970a2119b52588114
sopiko abulidze <abulidze2019@gmail.com>
2024-10-13 14:14:42 +0000

    add basic html structure and css styiling to categories file

--- job_me/views.py
+++ job_me/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "job_me/home.html")
+
+
+def categories(request):
+    return render(request, "job_me/categories.html")
+
