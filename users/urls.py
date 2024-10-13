1e40c95093e5fb08e2571e217b9699d6f965d34d
Viktor Rura <ruravictor@gmail.com>
2024-10-13 16:25:44 +0100

    Add modal logout and add functionality to navbar

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "users"
+
 urlpatterns = [
     path("profile/", views.profile, name="profile"),
+    path("accounts/logout/", views.custom_logout, name="logout"),
 ]
