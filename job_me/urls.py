b0a1c46559c90f5de5b0a760ab87902aab29fd94
Shaun Anderton <shaunanderton1@gmail.com>
2024-10-16 10:18:27 +0000

    second pass of main page

--- job_me/urls.py
+++ job_me/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import trigger_500_error
+# from .views import trigger_500_error
 
 app_name = "job_me"
 
@@ -8,6 +8,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("categories/", views.categories, name="categories"),
     path("about/", views.about, name="about"),
-    path('trigger-500/', trigger_500_error),
+    # path('trigger-500/', trigger_500_error),
 
 ]
