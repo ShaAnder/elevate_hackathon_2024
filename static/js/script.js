923a3445c65336d0bf51734f7765f8cb82c3a5dd
Shaun Anderton <shaunanderton1@gmail.com>
2024-10-16 01:03:44 +0000

    add additional about styling, responsiveness first pass

--- static/js/script.js
+++ static/js/script.js
@@ -6,7 +6,11 @@ const leftArrow = document.querySelector(".swiper-button-prev")
 const rightArrow = document.querySelector(".swiper-button-next")
 
 let slidesArr = Array.from(slideSections)
-console.log(slidesArr)
+for(i in slidesArr) {
+    if(i > 0) {
+        slidesArr[i].style.display = "none"
+    }
+}
 
 var sectionIndex = 0;
 
@@ -17,23 +21,20 @@ rightArrow.addEventListener("click", function() {
     if (sectionIndex == 3) {
         return
     } else {
-        if(sectionIndex != slidesArr[sectionIndex]) {
-            slidesArr[sectionIndex].style.display = "none"
-        } 
+        slidesArr[sectionIndex].style.display = "none"
         sectionIndex = sectionIndex +1
+        slidesArr[sectionIndex].style.display = "flex"
     }
 
 })
 
 leftArrow.addEventListener("click", function() {
-    if (sectionIndex <= -1) {
+    if (sectionIndex == 0) {
         return
     } else {
-        if(sectionIndex != slidesArr[sectionIndex]) {
-            slidesArr[sectionIndex].style.display = "hide"
-            sectionIndex = sectionIndex -1
-            slidesArr[sectionIndex].style.display = "flex"
-        } 
-
+        slidesArr[sectionIndex].style.display = "none"
+        sectionIndex = sectionIndex -1
+        slidesArr[sectionIndex].style.display = "flex"
     }
-})
+    }
+)
